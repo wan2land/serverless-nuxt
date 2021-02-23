69908ad960b15d4b527cb8af84451dd74dcac66f
Changwan Jun <wan2land@gmail.com>
2021-02-23 22:34:29 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,27 @@
+
+
+module.exports = {
+  telemetry: false,
+  head: {
+    title: 'serverless-sample',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  css: [
+  ],
+  plugins: [
+  ],
+  components: true,
+  buildModules: [
+  ],
+  modules: [
+  ],
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    Hello Serverless :-)
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
